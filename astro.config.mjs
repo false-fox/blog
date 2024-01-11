49f6f0f2df1de79ec5b3c4bfb18bddd99b80ae70
false-fox <154926234+false-fox@users.noreply.github.com>
2024-01-10 21:50:08 -0800

    YES

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+import pagefind from "astro-pagefind"
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://falsefox.dev',
+  integrations: [sitemap(), pagefind()]
+});
